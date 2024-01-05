e2e757d73d54b66a464733b85a39cb621464c98e
Srikanthh-21 <Srikanthpaindla21@gmail.com>
2024-01-05 18:50:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   //Step 1 - Make the get route work and render the index.ejs file
-  res.render("index.ejs");
+  res.render("views/index.ejs");
 
 });
 
